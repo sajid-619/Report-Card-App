2dfa811818822db15df1b4bea3e924af3f5428f0
Sajid Zaman <sajid-619@users.noreply.github.com>
2016-07-04 21:20:45 +0600

    Update TestRow.java

--- app/src/main/java/com/example/reportcard/TestRow.java
+++ app/src/main/java/com/example/reportcard/TestRow.java
@@ -24,6 +24,42 @@ public class TestRow {
     public int getTestMaxMarks(){
         return testMaxMarks;
     }
+    
+    @Override
+    public String toString() {
+        return "ReportCard{" +
+                "Grade='" + Grade + '\'' +
+                ", Comments='" + Comments + '\'' +
+                ", Average=" + Average +
+                ", subject='" + subject + '\'' +
+                ", term='" + term + '\'' +
+                ", studentName='" + studentName + '\'' +
+                '}';
+    }
+
+    /**
+     * Calculates a students grade based on homeworks, exams, and participation
+     *
+     * @return grade
+     */
+    public double calculateGrade() {
+
+        double grade = 0;
+        return grade;
+    }
+
+    /**
+     * Calculates a students average by adding all the grades from each class and dividing by the number of classes
+     *
+     * @return average
+     */
+    public double calculateAverage() {
+        double average = 0;
+        return average;
+    }
+
+
+}
 
     public void setTestName(String testName) {
         this.testName = testName;
